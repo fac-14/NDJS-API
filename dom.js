c804e6c40eed5f37f09eb16d3605f2936c345845
Simon Dupree <dupreenator@gmail.com>
2018-07-18 15:46:00 +0100

    dom_logic

--- dom.js
+++ dom.js
@@ -12,22 +12,41 @@ reders returned stats to page
 // 2. Logic.js will process the data.js object and return our filtered object containing stats, images...
 // 3. Dom.js will use the processed object and fills the HTML
 
-// Set up Input variables
-// var userNameOne
-// var animalOne
-// var userNameTwo
-// var animalTwo
+var form = document.querySelector("#form");
 
-// Set up Submit variable
-// var submitButton
+var gifDiv1 = document.querySelector("#avatar-output-1");
+var gifDiv2 = document.querySelector("#avatar-output-2");
 
-// Set up Output variables
-// var cardOne
-// var cardTwo
+var statDiv1 = document.querySelector("#stats-output-1");
+var statDiv2 = document.querySelector("#stats-output-2");
 
-// Set up EventListeners
-// submitButton.addEventlistener('submit', getData)
+// Extract input variables from form variable and send them to data.js
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  // grab form inputs
+  var usernameOne = event.target[0].value;
+  var animalOne = event.target[1].value;
+  var usernameTwo = event.target[2].value;
+  var animalTwo = event.target[3].value;
+  // console.log(usernameOne, usernameTwo, animalOne, animalTwo);
+  // make giphy requests
+  var gifUrlOne = getGif(animalOne);
+  var gifUrlTwo = getGif(animalTwo);
+  // retrieve stats object
+  var statsObjOne = getStatsAll(getGithub(usernameOne)); // logic.js, data.js
+  var statsObjTwo = getStatsAll(getGithub(usernameTwo)); // logic.js, data.js
+  // render gifs to page
+  renderGif(gifUrlOne, gifDiv1);
+  renderGif(gifUrlTwo, gifDiv2);
+  // render stats
+  renderStats(statsObjOne, statDiv1);
+  renderStats(statsObjTwo, statDiv2);
+});
 
-// Set up Functions
-//function getData (github, giphy)
-//update/render functions
+function renderGif(url, element) {
+  // set img source to url
+}
+
+function renderStats(obj, element) {
+  // create html with object, render to element
+}
