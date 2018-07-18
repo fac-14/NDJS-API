16459210c47a4b943159055ec2dd9fdfd26fb51c
Dominic Coelho <domwork@live.com>
2018-07-18 16:51:41 +0100

    reusable API func, functions that create URLs, placeholder callback

--- data.js
+++ data.js
@@ -10,3 +10,47 @@ XHR 2
     * gets stats using functions from logic.js...
 
 */
+
+// Variables for github name and search created in dom.js. Then will be passed in as arguments to the respective functions in this script
+var giphyURL = "";
+var githubURL = "";
+
+// API keys
+var giphy_api_key = "7cWhgewjrS2GSML9Sj3iCQZd24zYFLSh"; // Nathalie's API key! "Don't abuse it" (or try to! Nathalie DARES YOU)
+var github_api_key = "7030c53467665997aca886181fcf200c3e9f80a0"; // Dom's API key!
+
+// Functions to make the above URLs
+function createGiphyURL (searchTerm){
+    var searchTermSanitised = searchTerm.replace(/[^A-Za-z0-9 ]/g,"").replace(" ","+").trim(); //WOOO
+    giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTermSanitised + "&api_key=" + giphy_api_key;
+    return giphyURL;
+}
+
+function createGithubURL (username){
+    githubURL = "http://api.github.com/users/" + username + "/repos?access_token=" + github_api_key;
+    return githubURL;
+}
+
+// Reusable API request function
+function fetch (urlFunc, userInput, callback) {
+    var xhr = new XMLHttpRequest();
+  
+    xhr.addEventListener('load', function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        var response = JSON.parse(xhr.responseText);
+        return callback(response);
+      }
+    });
+  
+    xhr.open('GET', urlFunc(userInput));
+    xhr.send();
+  }
+
+
+  function testCallBack(response){
+    console.log(response);
+}
+
+  // Example calling of functions (comment it out!):
+//   fetch(createGithubURL, virtualDOMinic, testCallBack);
+
