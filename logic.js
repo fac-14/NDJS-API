07f352ebda7b6d901425f36d391f8405162983e1
Jessie Beech <jessie@jessiebeech.com>
2018-07-18 14:42:42 +0100

    added helper notes to logic.js

--- logic.js
+++ logic.js
@@ -1,6 +1,26 @@
-/*
-has functions that take data input and return stats.
-e.g.
-  function getLongestRepoName() {}
-*/
 
+// This file is a group of functions that will generate individual stats for a github user.
+// All should return a stat value e.g. numberOfRepos(obj) // > 25
+// The final function will aggregate & call all the other functions and return an OBJECT with key value pairs for each stat:
+
+// e.g 
+// userStats = {
+//   longestRepo: 25,
+//   numberOfStars: 6
+// }
+
+// e.g 
+// function longestRepoName(obj) {
+//    loop through repos, look for longest name
+//    return "Longest repo: " + length + " letters";
+// }
+
+
+// function getAllStats(data) {
+//   var output = {};
+//   output.logestRepo = longestRepoName(data);
+//   ... 
+//   return output;
+// }
+
+module.exports = getAllStats;
