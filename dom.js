8d163c94d0e584fda38b8df156ce46356891d8f6
Dominic C <domwork@live.com>
2018-07-18 12:25:06 +0100

    added desc

--- dom.js
+++ dom.js
@@ -1,1 +1,9 @@
+/*
+event listener for submit button
+retrieves user input - stores.
+then hides/disables input fields
+calls XHR requests through data.js
+renders returned gif to page
+reders returned stats to page
+*/
 
