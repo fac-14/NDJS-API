ef942cdc040a4536d1f319033004d764380d9996
Dominic C <domwork@live.com>
2018-07-18 12:21:50 +0100

    empty init

---
+++ data.js
@@ -0,0 +1,12 @@
+/*
+Compiles urls based on user input from dom.js
+
+XHR 1
+    * get single random gif from giphy, return url.gif
+
+XHR 2
+    * get data from github.com/username/repos
+    * return parsed object
+    * gets stats using functions from logic.js...
+
+*/
